1449b005e6a778884b06d0c5972e4114b3f26a18
Xenko10 <kubinios_2002@o2.pl>
2024-02-23 09:47:09 +0100

    change position of left content in hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,15 +4,17 @@ export default function Home() {
   return (
     <div className={styles.home}>
       <div className={styles.mainContent}>
-        <h1>
-          <div className={styles.greyH1}>Lessons and insights </div>
-          <div className={styles.greenH1}>from 8 years</div>
-        </h1>
+        <div className={styles.leftContent}>
+          <h1>
+            <div className={styles.greyH1}>Lessons and insights </div>
+            <div className={styles.greenH1}>from 8 years</div>
+          </h1>
+          <p>
+            Where to grow your business as a photographer: site or social media?
+          </p>
+          <button>Register</button>
+        </div>
         <img src='/hero.png' alt='IT specialist' />
-        <p>
-          Where to grow your business as a photographer: site or social media?
-        </p>
-        <button>Register</button>
       </div>
       <div className={styles.dots}>
         <div className={`${styles.dot} ${styles.active}`}></div>
