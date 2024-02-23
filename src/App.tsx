1b30cdb2f391955d6f0235b5a5c9d78a6da6c6b1
Xenko10 <kubinios_2002@o2.pl>
2024-02-23 10:47:11 +0100

    add client section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header.tsx";
 import Hero from "./components/Hero/Hero.tsx";
+import Clients from "./components/Clients/Clients.tsx";
 
 export default function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Clients />
     </>
   );
 }
---
+++ src/components/Clients/Clients.tsx
@@ -0,0 +1,29 @@
+import styles from "./Clients.module.css";
+
+const clientLogos = [
+  "logo1.png",
+  "logo2.png",
+  "logo3.png",
+  "logo4.png",
+  "logo5.png",
+  "logo6.png",
+  "logo7.png",
+];
+
+export default function Clients() {
+  return (
+    <div className={styles.clients}>
+      <h2>Our Clients</h2>
+      <p>We have been working with some Fortune 500+ clients</p>
+      <div className={styles.imageWrapper}>
+        {clientLogos.map((logo, index) => (
+          <img
+            key={index}
+            src={`./clients/${logo}`}
+            alt={`Client ${index + 1}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
