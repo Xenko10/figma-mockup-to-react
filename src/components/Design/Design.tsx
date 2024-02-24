546731c5f8bd3bd24cc1a59a48c5b18f6d71ab19
Xenko10 <kubinios_2002@o2.pl>
2024-02-24 11:23:07 +0100

    finish design section

--- src/components/Design/Design.tsx
+++ src/components/Design/Design.tsx
@@ -3,7 +3,21 @@ import styles from "./Design.module.css";
 export default function Design() {
   return (
     <div className={styles.design}>
-      <h1>Design</h1>
+      <img src='./img/design.png' alt='mobile login' />
+      <div className={styles.content}>
+        <h2>How to design your site footer like we did</h2>
+        <p>
+          Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor,
+          augue nec tincidunt molestie, massa nunc varius arcu, at scelerisque
+          elit erat a magna. Donec quis erat at libero ultrices mollis. In hac
+          habitasse platea dictumst. Vivamus vehicula leo dui, at porta nisi
+          facilisis finibus. In euismod augue vitae nisi ultricies, non aliquet
+          urna tincidunt. Integer in nisi eget nulla commodo faucibus efficitur
+          quis massa. Praesent felis est, finibus et nisi ac, hendrerit
+          venenatis libero. Donec consectetur faucibus ipsum id gravida.
+        </p>
+        <button>Learn More</button>
+      </div>
     </div>
   );
 }
