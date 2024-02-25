a5919a671d2ba3123105083125997ae95d800c87
Xenko10 <kubinios_2002@o2.pl>
2024-02-25 18:54:02 +0100

    add max-width in all components

--- src/components/Achievements/Achievements.tsx
+++ src/components/Achievements/Achievements.tsx
@@ -3,40 +3,42 @@ import styles from "./Achievements.module.css";
 export default function Achievements() {
   return (
     <div className={styles.achievements}>
-      <div className={styles.leftContent}>
-        <h2>
-          Helping a local
-          <div className={styles.greenH2}> business reinvent itself</div>
-        </h2>
-        <p>We reached here with our hard work and dedication</p>
-      </div>
-      <div className={styles.rightContent}>
-        <div className={styles.item}>
-          <img src='./img/achievements/icon1.png' alt='members' />
-          <div>
-            <h3>2,245,341</h3>
-            <p>Members</p>
-          </div>
+      <div className={styles.contentWrapper}>
+        <div className={styles.leftContent}>
+          <h2>
+            Helping a local
+            <div className={styles.greenH2}> business reinvent itself</div>
+          </h2>
+          <p>We reached here with our hard work and dedication</p>
         </div>
-        <div className={styles.item}>
-          <img src='./img/achievements/icon2.png' alt='clubs' />
-          <div>
-            <h3>46,328</h3>
-            <p>Clubs</p>
+        <div className={styles.rightContent}>
+          <div className={styles.item}>
+            <img src='./img/achievements/icon1.png' alt='members' />
+            <div>
+              <h3>2,245,341</h3>
+              <p>Members</p>
+            </div>
           </div>
-        </div>
-        <div className={styles.item}>
-          <img src='./img/achievements/icon3.png' alt='event bookings' />
-          <div>
-            <h3>828,867</h3>
-            <p>Event Bookings</p>
+          <div className={styles.item}>
+            <img src='./img/achievements/icon2.png' alt='clubs' />
+            <div>
+              <h3>46,328</h3>
+              <p>Clubs</p>
+            </div>
           </div>
-        </div>
-        <div className={styles.item}>
-          <img src='./img/achievements/icon4.png' alt='payments' />
-          <div>
-            <h3>1,926,436</h3>
-            <p>Payments</p>
+          <div className={styles.item}>
+            <img src='./img/achievements/icon3.png' alt='event bookings' />
+            <div>
+              <h3>828,867</h3>
+              <p>Event Bookings</p>
+            </div>
+          </div>
+          <div className={styles.item}>
+            <img src='./img/achievements/icon4.png' alt='payments' />
+            <div>
+              <h3>1,926,436</h3>
+              <p>Payments</p>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,23 +3,26 @@ import styles from "./Hero.module.css";
 export default function Home() {
   return (
     <div className={styles.home}>
-      <div className={styles.mainContent}>
-        <div className={styles.leftContent}>
-          <h1>
-            <span className={styles.greyH1}>Lessons and insights </span>
-            <div className={styles.greenH1}>from 8 years</div>
-          </h1>
-          <p>
-            Where to grow your business as a photographer: site or social media?
-          </p>
-          <button>Register</button>
+      <div className={styles.contentWrapper}>
+        <div className={styles.mainContent}>
+          <div className={styles.leftContent}>
+            <h1>
+              <span className={styles.greyH1}>Lessons and insights </span>
+              <div className={styles.greenH1}>from 8 years</div>
+            </h1>
+            <p>
+              Where to grow your business as a photographer: site or social
+              media?
+            </p>
+            <button>Register</button>
+          </div>
+          <img src='./img/hero.png' alt='IT specialist' />
         </div>
-        <img src='./img/hero.png' alt='IT specialist' />
-      </div>
-      <div className={styles.dots}>
-        <div className={`${styles.dot} ${styles.active}`}></div>
-        <div className={styles.dot}></div>
-        <div className={styles.dot}></div>
+        <div className={styles.dots}>
+          <div className={`${styles.dot} ${styles.active}`}></div>
+          <div className={styles.dot}></div>
+          <div className={styles.dot}></div>
+        </div>
       </div>
     </div>
   );
